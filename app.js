14b1c4254c33bf69043806cfb81154e2f0fc9232
dpirvoiu <adisor.pdw@gmail.com>
2023-06-01 15:18:39 +0300

    chore: add middleware and db

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
-require('dotenv');
-const connect = require('./db/connect')
+
+// DATABASE
+const connectDB = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 const app = express();
+
+// Use middleware to see json object
+app.use(express.json())
+
+// Home route
+app.get('/', (req, res)=>{
+  res.send('e-commerce api')
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
 const start = async  () =>{
   try{
 
-    connect(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`listening to ${port}...`))
-    
+
   }catch (error) {
      console.log(error)
   }
